e0d17b2e1fab019206fd4b5d7c360e0fa161185b
FabioRossiArcetri <rossi76@gmail.com>
2025-09-05 16:00:22 +0200

    (FRO) Only the process with rank 0 should build the diagram.

--- specula/simul.py
+++ specula/simul.py
@@ -896,7 +896,7 @@ class Simul():
         self.create_input_list_inputs(params)
         self.connect_objects(params)
 
-        if self.diagram or self.diagram_filename or self.diagram_title:
+        if process_rank==0 and self.diagram or self.diagram_filename or self.diagram_title:
             if self.diagram_filename is None:
                 self.diagram_filename = str(Path(self.param_files[0]).with_suffix('.png'))
             if self.diagram_title is None:
