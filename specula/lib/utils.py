2a8f3a2f1ddb6c22f596ab41ac5c3a809e8c1b5a
Alfio Puglisi <alfio.puglisi@inaf.it>
2026-04-10 19:11:17 +0200

    Simplified flatten lib function (#597)

--- specula/lib/utils.py
+++ specula/lib/utils.py
@@ -339,3 +339,21 @@ def make_subpixel_shift_phase(shape, xp, dtype,
 
 def make_tn():
     return time.strftime("%Y%m%d_%H%M%S")
+
+
+def flatten(x):
+    '''
+    Generator that will flatten a list that may contain
+    other lists (nested arbitrarily) and simple items
+    into a flat list.
+
+    >>> flat = flatten([[1,[2,3]],4,[5,6]])
+    >>> list(flat)
+    [1,2,3,4,5,6]
+
+    '''
+    for item in x:
+        try:
+            yield from flatten(item)
+        except TypeError:
+            yield item
